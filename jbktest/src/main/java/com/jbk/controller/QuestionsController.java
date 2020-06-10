c421d9422b8846a64bde0dca4ec9796a9d8a14ab
User-pc <User-pc@VIVEK>
2020-06-11 00:36:32 +0530

    api mapping....

--- jbktest/src/main/java/com/jbk/controller/QuestionsController.java
+++ jbktest/src/main/java/com/jbk/controller/QuestionsController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("testapi")
+@RequestMapping("/api")
 public class QuestionsController {
 	
 	@RequestMapping("singleQuestion")
